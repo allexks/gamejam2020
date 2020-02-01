f9242520c25cc40459bef6514163958ae066de06
Alexander Ignatov <ani1802@abv.bg>
2020-02-01 02:33:38 +0200

    Create bomb and bullets

---
+++ Levels/Bomb.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+signal detonation_ended
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("pesho")
+	$BoomCollision.disabled = true
+
+
+func _on_FallTimer_timeout():
+	$BoomCollision.disabled = false
+	$EndOfDamageTimer.start()
+
+
+func _on_EndOfDamageTimer_timeout():
+	$BoomCollision.disabled = true
+	emit_signal("detonation_ended")
